83e66d48a6a73f4869098dd4765fbe8fd7949542
Bang Do - bdo023 <wktfirefly283@gmail.com>
2025-07-02 02:42:12 -0700

    Updated Elib section

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -15,10 +15,10 @@ const projects = [
     id: 2,
     title: "E-Library Dashboard",
     description:
-      "Designed an Interactive dashboard that helps share E-books with optimized pagination and filtering capabilities. Testing and using different techniques such as debouncing and rate limiting to improve performance of the live search feature. Fully utilizing AWS services such as EC2 and S3 for hosting and file storage. Significantly improved the performance of backend server by deploying on an EC2 instance with Ubuntu.",
+      "Designed an Interactive dashboard that helps share E-books with optimized pagination and filtering capabilities. Testing and using different techniques such as debouncing and rate limiting to improve performance of the live search feature. Fully utilizing AWS services such as EC2 and S3 for hosting and file storage. Significantly improved the performance of backend server by deploying on an EC2 instance with Ubuntu. (currently disabled due to charges)",
     image: "/projects/ELibImg.JPG",
     tags: ["React", "NodeJS", "Prisma", "PostgreSQL", "EC2", "S3", "AWS"],
-    demoUrl: "https://e-library-dashboard-fe-deployed.vercel.app/",
+    //demoUrl: "https://e-library-dashboard-fe-deployed.vercel.app/",
     githubUrl: "https://github.com/FireFly0000/E-Library-Dashboard",
   },
   {
